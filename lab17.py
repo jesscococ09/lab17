eb7586fd6439c2ab0a781321f057755f35cb9333
jess <jesstorrealba@gmail.com>
2024-11-05 23:14:57 -0800

    first commit

---
+++ lab17.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, flash, redirect
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csumb-otter'
+bootstrap = Bootstrap5(app)
+
+class Playlist(FlaskForm):
+    song_title = StringField(
+        'Song Title', 
+        validators=[DataRequired()]
+    )
+class Genre(FlaskForm):
+    genre_fill = StringField(
+        'genre', 
+        validators=[DataRequired()]
+    )
+
+playlist = []
+genre= []
+
+def store_song(my_song):
+    playlist.append(dict(
+        song = my_song,
+        date = datetime.today()
+    ))
+def store_genre(my_genre):
+    genre.append(dict(
+        genre= my_genre,
+        date= datetime.today()
+    ))
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = Playlist()
+    formg= Genre()
+    if form.validate_on_submit():
+        store_song(form.song_title.data)
+        return redirect('/view_playlist')
+    if formg.validate_on_submit():
+        store_genre(formg.genre_fill.data)
+        return redirect('/view_playlist')
+    return render_template('index.html', form=form, form=formg)
+
+@app.route('/view_playlist')
+def vp():
+    return render_template('vp.html', playlist=playlist, genre=genre)
